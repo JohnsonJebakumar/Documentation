d7908f1838054ab95402182a579f9a1a6f57f261
JohnsonJebakumar <gjohnsonmech16@gmail.com>
2017-06-05 17:53:23 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -37,7 +37,7 @@
 			//console.log(crmcsrCookie)
 			var http = new XMLHttpRequest();
 			http.open( method , url , true );
-			//http.setRequestHeader("X-ZCSRF-TOKEN", "crmcsrfparam=fea951d66bd9140dcd5c63f15f6209de850c1c6f02aeabc0c9811bc501cb977eb7d5c69d4caf60632e00903dbf7c865febfb2020255815a4934cc65feff2e964");
+			http.setRequestHeader("X-ZCSRF-TOKEN", "crmcsrfparam=fea951d66bd9140dcd5c63f15f6209de850c1c6f02aeabc0c9811bc501cb977eb7d5c69d4caf60632e00903dbf7c865febfb2020255815a4934cc65feff2e964");
 			http.onreadystatechange = function() {
 			    if(http.readyState == 4 && http.status == 200) {
 				console.log(http.responseText);
